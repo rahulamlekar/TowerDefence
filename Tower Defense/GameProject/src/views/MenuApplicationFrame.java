1b24125736d43dffa7963b7247a098a64283ef53
Asher Wright <sastreen@hotmail.com>
2015-04-05 17:52:24 -0300

    Added option to load a map from file
    Exactly what the title says.

--- Tower Defense/GameProject/src/views/MenuApplicationFrame.java
+++ Tower Defense/GameProject/src/views/MenuApplicationFrame.java
@@ -6,8 +6,10 @@ import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import models.TDMap;
 
@@ -16,10 +18,12 @@ public class MenuApplicationFrame extends JFrame implements ActionListener{
 	public static final int PIXELHEIGHT=300;
 	public static final String APP_NAME = "Main Menu";
 	public static final int TIMEOUT = 30 ;
+	final JFileChooser fc = new JFileChooser();
 	JPanel mainPanel = new JPanel();
 	JButton bPlay = new JButton("Play a game");
 	JButton bCreateMap = new JButton("Create a map");
 	JButton bQuit = new JButton("Quit");
+	JButton bLoadMap = new JButton("Load a map");
 	TDMap mapToLoad;
 	//MainMenuActivity activity;
 	
@@ -28,7 +32,10 @@ public class MenuApplicationFrame extends JFrame implements ActionListener{
 		bPlay.addActionListener(this);
 		bCreateMap.addActionListener(this);
 		bQuit.addActionListener(this);
+		bLoadMap.addActionListener(this);
 		mapToLoad = new TDMap("res/DIRTMAP1.TDMap"); //set default map
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Map Files", "TDMap");
+		fc.setFileFilter(filter);
 	}
 
 	@Override
@@ -42,6 +49,14 @@ public class MenuApplicationFrame extends JFrame implements ActionListener{
 				new MapEditorApplicationFrame();
 			}else if(e.getSource() == bQuit){
 				System.exit(0);
+			}else if(e.getSource() == bLoadMap){
+				int returnVal = fc.showOpenDialog(this);
+		        if (returnVal == JFileChooser.APPROVE_OPTION) {
+		            String filePath = fc.getSelectedFile().getPath();
+		            mapToLoad = new TDMap(filePath);
+		        } else {
+		        	
+		        }
 			}
 	}
 		
@@ -55,6 +70,7 @@ public class MenuApplicationFrame extends JFrame implements ActionListener{
         mainPanel.requestFocus();
         mainPanel.add(bPlay);
         mainPanel.add(bCreateMap);
+        mainPanel.add(bLoadMap);
         mainPanel.add(bQuit);
         
 		//set the Frame properties
